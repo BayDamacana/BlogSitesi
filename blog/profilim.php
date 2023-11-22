4db1fb82659dbd6957746dec3dae30757854bf44
BayDamacana <119080404+BayDamacana@users.noreply.github.com>
2023-11-22 22:38:14 +0300

    Add files via upload

---
+++ blog/profilim.php
@@ -0,0 +1,21 @@
+<?php
+include("header.php");
+?>
+
+<div class="container">
+ <h2 style="text-align:center;">Ben Kimim</h2>
+   
+ <p>
+Ben Yasin Abacı 
+
+Hataylıyım
+
+ESOGÜ Sivrihisar MYO'da bilgisayar programcılığı 2. sınıf öğrencisiyim
+
+Benimle yasinabaci@outlook.com üzerinden iletişime geçbilirsiniz
+        Ğ
+</p>
+ </div>
+</body>
+
+</html>
---
+++ blog/sonuc.php
@@ -0,0 +1,15 @@
+<?php
+include("header.php");
+
+$aramasorgusu = @mysql_real_escape_string($_GET['aramasorgusu']);
+$sonucsorgu = @mysql_query("SELECT * FROM konular WHERE baslik LIKE '%".$aramasorgusu."%'" );
+if(@mysql_num_rows($sonucsorgu)>0){
+ while($sorguoku=@mysql_fetch_array($sonucsorgu)){
+  echo $sorguoku['baslik'].'<br>';
+ }
+}
+else{
+ echo 'Aradığınız İçerik Bulunamadı';
+}
+?>
+
